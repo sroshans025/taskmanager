2876270feb48e07170b79f6f99c595616b030898
Roshan S <sroshans025@gmail.com>
2026-04-18 23:17:02 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app = FastAPI(title="FastAPI Task Manager")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
